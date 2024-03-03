a645f01296413d766957f1daa9af96d1a5fcbb19
hMoffat <hmoffat.work@gmail.com>
2024-03-03 19:32:39 +0000

    Add filter toggle to nav.

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,10 +1,34 @@
 import { Link } from "react-router-dom";
 import UserContext from "./UserContext";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import homeSymbol from "../assets/icons8-home.svg";
 
-export default function NavBar() {
+export default function NavBar({ setFilter, filter }) {
   const { loggedInUser } = useContext(UserContext);
+  const [width, setWidth] = useState(
+    window.innerWidth > 480 ? "desktop" : "mob"
+  );
+
+  useEffect(() => {
+    const handleWidth = () => {
+      const oldSize = width;
+      setWidth(window.innerWidth > 480 ? "desktop" : "mob");
+      if (window.innerWidth <= 480) setFilter(true);
+      if (window.innerWidth > 480 && oldSize !== "desktop") {
+        setFilter(false);
+      }
+    };
+    window.addEventListener("resize", handleWidth);
+
+    return () => {
+      window.removeEventListener("resize", handleWidth);
+    };
+  }, []);
+
+  // const toggleFilter = () => {
+  //   setFilter(filter ? false : true)
+  // }
+
   return (
     <nav className="nav-bar">
       <Link to="/" className="nav-link">
@@ -19,6 +43,15 @@ export default function NavBar() {
         />
         <p>You</p>
       </Link>
+      {width === "desktop" && (
+        <button
+          onClick={() => {
+            setFilter(!filter);
+          }}
+        >
+          {filter ? `Hide` : `Show`} article filter options
+        </button>
+      )}
     </nav>
   );
 }
